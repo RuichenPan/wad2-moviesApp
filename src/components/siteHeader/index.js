9f3f6815d93ab63d97dd0a222c61f9bcc47cbe49
Ruichen Pan <3196968196@qq.comm>
2020-12-11 16:41:13 +0000

    Change the TMDB logo

--- src/components/siteHeader/index.js
+++ src/components/siteHeader/index.js
@@ -9,8 +9,8 @@ const SiteHeader = () => {
     <nav className="navbar  navbar-light fixed-top  bg-dark ">
       <nav className="navbar-brand text-white">
         <Link className=" text-white" to="/">
-          TMDB Client
-        </Link>
+        <img src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg"  width="200" height="50" />
+          </Link>
       </nav>
       <FontAwesomeIcon
         className="navbar-text text-light"
@@ -56,16 +56,6 @@ const SiteHeader = () => {
           <li className="nav-item">
             <Link className="nav-link text-white" to="/movies/nowplaying">
               Now Playing
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link text-white" to="/actors">
-              Popular Actors
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link text-white" to="/actors/favorites">
-              Favorite Actors
             </Link>
           </li>
         </ul>
